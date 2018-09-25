33eb9632e145d0c6196c337625855847f3e9502e
Can Sahin <cann2005@gmail.com>
2018-09-25 14:22:24 +0300

    minor changes

--- internals/templates/containers/NotFoundPage/index.tsx
+++ internals/templates/containers/NotFoundPage/index.tsx
@@ -7,7 +7,6 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import H1 from 'components/H1';
 import messages from './messages';
 
 export default function NotFound() {

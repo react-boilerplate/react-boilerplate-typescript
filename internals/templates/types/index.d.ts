411b97de1408c3910464d71d4ece982350f68585
Can Sahin <cann2005@gmail.com>
2018-12-23 14:31:09 +0300

    ‘npm run clean’ bugs fixed

---
+++ internals/templates/types/index.d.ts
@@ -0,0 +1,28 @@
+import { Reducer, Store } from 'redux';
+import { RouterState } from 'connected-react-router';
+import { ILanguageProviderProps } from 'containers/LanguageProvider';
+
+export interface LifeStore extends Store<{}> {
+  injectedReducers?: any;
+  injectedSagas?: any;
+  runSaga(saga: () => IterableIterator<any>, args: any): any;
+}
+
+export interface InjectReducerParams {
+  key: keyof ApplicationRootState;
+  reducer: Reducer<any, any>;
+}
+
+export interface InjectSagaParams {
+  key: keyof ApplicationRootState;
+  saga: () => IterableIterator<any>;
+  mode?: string | undefined;
+}
+
+// Your root reducer type, which is your redux state types also
+export interface ApplicationRootState {
+  readonly router: RouterState;
+  readonly language: ILanguageProviderProps;
+  // for testing purposes
+  readonly test: any;
+}

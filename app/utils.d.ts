15bd8e3ec727818d7695f8b4d6809dc662b47325
Can Sahin <cann2005@gmail.com>
2019-03-22 17:54:25 +0300

    type definition moved to correct file

--- app/utils.d.ts
+++ app/utils.d.ts
@@ -12,6 +12,12 @@ declare module '*.jpg' {
   const content: string | any;
   export default content;
 }
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
 // declare module '*!raw' {
 //   const content: string|any;
 //   export default content;

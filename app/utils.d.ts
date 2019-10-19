c3dd70670d7f935640eb74f19594991a0773a440
Gray Strider <zhoga.ivan@gmail.com>
2019-10-19 13:41:57 +1100

    update #37, remove unnecessary declarations

--- app/utils.d.ts
+++ app/utils.d.ts
@@ -1,24 +1,5 @@
-// declare module '*.css' {
-//   const content: string|any;
-//   export default content;
-// }
+declare module '*.svg';
 
-declare module '*.svg' {
-  const content: string | any;
-  export default content;
-}
+declare module '*.jpg';
 
-declare module '*.jpg' {
-  const content: string | any;
-  export default content;
-}
-
-declare module '*.png' {
-  const content: any;
-  export default content;
-}
-
-// declare module '*!raw' {
-//   const content: string|any;
-//   export default content;
-// }
+declare module '*.png';

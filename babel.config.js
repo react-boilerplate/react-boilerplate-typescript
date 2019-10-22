080f599506be964dc1ef1e694b6df87031d84da1
Gray Strider <zhoga.ivan@gmail.com>
2019-10-22 20:37:40 +1100

    remove @babel/typescript

--- babel.config.js
+++ babel.config.js
@@ -7,18 +7,11 @@ module.exports = {
       },
     ],
     '@babel/preset-react',
-    '@babel/typescript',
   ],
   plugins: [
     'styled-components',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
-    [
-      '@babel/plugin-transform-typescript',
-      {
-        allowNamespaces: true,
-      },
-    ],
   ],
   env: {
     production: {

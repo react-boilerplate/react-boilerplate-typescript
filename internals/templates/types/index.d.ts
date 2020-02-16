ca2fc39965c11ebf1a97bc8b4acc125b6f96e912
nazeh <ar.nazeh@gmail.com>
2020-02-16 10:54:14 +0300

    🚨 Update templates/types/index.d.ts

--- internals/templates/types/index.d.ts
+++ internals/templates/types/index.d.ts
@@ -1,6 +1,7 @@
 import { Reducer, Store } from 'redux';
 import { RouterState } from 'connected-react-router';
 import { ContainerState as LanguageProviderState } from 'containers/LanguageProvider/types';
+// [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating continers seamlessly
 
 export interface InjectedStore extends Store {
   injectedReducers: any;
@@ -23,6 +24,8 @@ export interface InjectSagaParams {
 export interface ApplicationRootState {
   readonly router: RouterState;
   readonly language: LanguageProviderState;
+  // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating continers seamlessly
+
   // for testing purposes
   readonly test: any;
 }

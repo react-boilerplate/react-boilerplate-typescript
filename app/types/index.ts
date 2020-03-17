9fa11ff47e9c253715ed3fa6e0409fdb2f259842
Can Sahin <cann2005@gmail.com>
2020-03-17 15:18:46 +0300

    package version updates
    - typescript, redux-sage and ts-loader updated
    - errors after packages updates fixed
    - incremental flag removed from tsconfig (its included in fork-ts-checker-webpack-plugin by default)

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,6 @@
 import { Reducer, Store } from 'redux';
 import { RouterState } from 'connected-react-router';
+import { Saga } from 'redux-saga';
 import { ContainerState as LanguageProviderState } from 'containers/LanguageProvider/types';
 import { ContainerState as AppState } from 'containers/App/types';
 import { ContainerState as HomeState } from 'containers/HomePage/types';
@@ -8,10 +9,7 @@ import { ContainerState as HomeState } from 'containers/HomePage/types';
 export interface InjectedStore extends Store {
   injectedReducers: any;
   injectedSagas: any;
-  runSaga(
-    saga: (() => IterableIterator<any>) | undefined,
-    args: any | undefined,
-  ): any;
+  runSaga(saga: Saga<any[]> | undefined, args: any | undefined): any;
 }
 
 export interface InjectReducerParams {
--- internals/templates/types/index.ts
+++ internals/templates/types/index.ts
@@ -1,15 +1,13 @@
 import { Reducer, Store } from 'redux';
 import { RouterState } from 'connected-react-router';
+import { Saga } from 'redux-saga';
 import { ContainerState as LanguageProviderState } from 'containers/LanguageProvider/types';
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
 export interface InjectedStore extends Store {
   injectedReducers: any;
   injectedSagas: any;
-  runSaga(
-    saga: (() => IterableIterator<any>) | undefined,
-    args: any | undefined,
-  ): any;
+  runSaga(saga: Saga<any[]> | undefined, args: any | undefined): any;
 }
 
 export interface InjectReducerParams {

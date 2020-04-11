dac0207b265a3e3202e998035144a2861d112a32
Austin Rivas <austinrivas@gmail.com>
2020-04-11 19:41:17 +0700

    LoadingIndicator no longer dynamically creates styled-components (#69)

--- app/components/LoadingIndicator/Circle.tsx
+++ app/components/LoadingIndicator/Circle.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import styled, { keyframes } from 'styles/styled-components';
 
+interface Props {
+  rotate?: number;
+  delay?: number;
+  id?: string;
+}
+
 const circleFadeDelay = keyframes`
   0%,
   39%,
@@ -13,40 +19,37 @@ const circleFadeDelay = keyframes`
   }
 `;
 
-interface Props {
-  rotate?: number;
-  delay?: number;
-  id?: string;
-}
-const Circle = (props: Props) => {
-  const CirclePrimitive = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    ${props.rotate &&
+const CirclePrimitive = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  ${(props: Props) =>
+    props.rotate &&
     `
       -webkit-transform: rotate(${props.rotate}deg);
       -ms-transform: rotate(${props.rotate}deg);
       transform: rotate(${props.rotate}deg);
     `} &:before {
-      content: '';
-      display: block;
-      margin: 0 auto;
-      width: 15%;
-      height: 15%;
-      background-color: #999;
-      border-radius: 100%;
-      animation: ${circleFadeDelay} 1.2s infinite ease-in-out both;
-      ${props.delay &&
+    content: '';
+    display: block;
+    margin: 0 auto;
+    width: 15%;
+    height: 15%;
+    background-color: #999;
+    border-radius: 100%;
+    animation: ${circleFadeDelay} 1.2s infinite ease-in-out both;
+    ${(props: Props) =>
+      props.delay && // eslint-disable-line indent
       `
         -webkit-animation-delay: ${props.delay}s;
         animation-delay: ${props.delay}s;
       `};
-    }
-  `;
-  return <CirclePrimitive />;
-};
+  }
+`;
+const Circle = (props: Props) => (
+  <CirclePrimitive rotate={props.rotate} delay={props.delay} />
+);
 
 export default Circle;
